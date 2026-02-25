65b50084e103516e1a2a50d0f64a234d2d36490e
Maxwell Aboagye <nanabaah12@gmail.com>
2026-02-25 12:51:27 +0100

    xcom auto and manual

---
+++ dags/5_XCOMs_kwargs.py
@@ -0,0 +1,37 @@
+from airflow.sdk import dag, task
+
+
+@dag(dag_id="xcoms_dag_manual")
+def xcoms_dag_manual(**kwargs):
+    @task.python
+    def first_task():
+        # Extracting "ti" from kwargs to push data to XComs manually
+        ti = kwargs["ti"]
+
+        print("Extracting data from the source...This is the first task!")
+        fetched_data = {"data": [1, 2, 3, 4, 5]}
+        ti.xcom_push(key="return_result", value=fetched_data)
+
+    @task.python
+    def second_task(**kwargs):
+        ti = kwargs["ti"]
+
+        fetched_data = ti.xcom_pull(task_ids="first_task", key="return_result")["data"]
+        transformed_data = [x * 2 for x in fetched_data]
+        transformed_data_dict = {"transformed_data": transformed_data}
+        ti.xcom_push(key="return_result", value=transformed_data_dict)
+
+    @task.python
+    def third_task(**kwargs):
+        ti = kwargs["ti"]
+        loaded_data = ti.xcom_pull(task_ids="second_task", key="return_result")["transformed_data"]
+        return loaded_data
+
+    # Defining task dependencies
+    first = first_task()
+    second = second_task(first)
+    third = third_task(second)
+
+
+# Running the DAG
+xcoms_dag_manual()
